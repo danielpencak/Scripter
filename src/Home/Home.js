ec50541f1b3bd20cc95aa0c5d15ae074b3a9e3ad
Daniel Pencak <danielpencak@gmail.com>
2017-03-20 10:14:49 -0700

    Close #11 Setup home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,45 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Home.css';
 import axios from 'axios';
 import { Grid, Row, Col, Glyphicon } from 'react-bootstrap'
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-    }
-
-  }
-
-  render() {
+export default function Home(props) {
     return (
       <div className="Home">
-        {/* <div className="hero"
-          style={{backgroundImage:'url(https://maps.googleapis.com/maps/api/staticmap?center=seattle&zoom=13&size=1200x300&scale=2&maptype=roadmap&key=AIzaSyCtST93n2hGc5ay2hN6DmtqnuB1zCKu1UA&style=feature:all|element:labels|visibility:off&style=feature:landscape.man_made%7Celement:geometry%7Ccolor:0xf7f1df&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0xd0e3b4&style=feature:landscape.natural.terrain%7Celement:geometry%7Cvisibility:off&style=feature:poi%7Celement:labels%7Cvisibility:off&style=feature:poi.business%7Cvisibility:off&style=feature:poi.medical%7Celement:geometry%7Ccolor:0xfbd3da&style=feature:poi.park%7Celement:geometry%7Ccolor:0xbde6ab&style=feature:road%7Celement:geometry.stroke%7Cvisibility:off&style=feature:road%7Celement:labels%7Cvisibility:off&style=feature:road.arterial%7Celement:geometry.fill%7Ccolor:0xffffff&style=feature:road.highway%7Celement:geometry.fill%7Ccolor:0xffe15f&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0xefd151&style=feature:road.local%7Celement:geometry.fill%7Ccolor:0xlack&style=feature:transit.station.airport%7Celement:geometry.fill%7Ccolor:0xcfb2db&style=feature:water%7Celement:geometry%7Ccolor:0xa2daf2)'}}
+        <div className="hero"
+          style={{backgroundImage:'url(/assets/images/screenplay.png)'}}
           >
             <div className="banner">
-              {this.state.heroString}
+              Writing the next big screenplay will take a team.
             </div>
           </div>
           <Grid>
             <Row>
               <Col sm={4}>
-                <Glyphicon glyph="map-marker" />
-                <div>We use maps to help you find people playing nearby</div>
+                <Glyphicon glyph="transfer" />
+                <div>Using data synchronization technology you can see a collaborator's edits as they make them.</div>
               </Col>
               <Col sm={4}>
-                <Glyphicon glyph="knight" />
-                <div>Discover new games that are popular in your area</div>
+                <Glyphicon glyph="film" />
+                <div>We take care of the formatting. That way you can focus on what will make your script stand out: the story.</div>
               </Col>
               <Col sm={4}>
-                <Glyphicon glyph="globe" />
-                <div>Explore your city and its unique places</div>
+                <Glyphicon glyph="pencil" />
+                <div>Since everyone on your team has the most recent version of the script at all times getting feedback and revisions is simpler than ever.</div>
               </Col>
             </Row>
-          </Grid> */}
+          </Grid>
           <small>© 2017 Scripter</small>
         </div>
       );
   }
-}
